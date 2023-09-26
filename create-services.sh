5349f6c587377220d0fe5011d0dd084fb9858bb2
void <yane.clarke@gmail.com>
2023-09-27 01:30:55 +1000

    Added Bash script to create a Thread-Per-URL Python web scraper, handle dependencies, and execute the scraping process. Updated average temps resolver functions and a getAllCities function on apollo server and added a line to auto-disable telemetry on mongodb container.

--- create-services.sh
+++ create-services.sh
@@ -117,12 +117,16 @@ import { ApolloServer } from '@apollo/server';
 import { startStandaloneServer } from '@apollo/server/standalone';
 import { MongoClient, Db } from 'mongodb';
 
+// Constants
 const { MONGO_URI = '', DB_NAME = 'recordsDB', PORT: PORT_ENV } = process.env;
 const PORT = parseInt(PORT_ENV, 10) || 4000;
+const DEFAULT_LIMIT = 10;
 
+// Database Variables
 let client: MongoClient;
 let db: Db;
 
+// Database Connection Functions
 async function connectToDatabase() {
     try {
         client = await MongoClient.connect(MONGO_URI);
@@ -138,194 +142,198 @@ async function disconnectFromDatabase() {
     try {
         await client?.close();
         console.log('Disconnected from MongoDB');
-    } catch (err) {
-        console.error("Error disconnecting from MongoDB:", err);
+    } catch (error) {
+        console.error("Error disconnecting from MongoDB:", error);
     }
 }
 
+// Utility Functions
 const buildFilter = (args: QueryArgs): object => {
     const filter: any = {};
-
-    if (args.dt_iso) filter.dt_iso = args.dt_iso;
-    if (args.startDate && args.endDate) filter.dt_iso = { $gte: args.startDate, $lte: args.endDate };
-    if (args.years) {
-        const yearsAgo = new Date();
-        yearsAgo.setFullYear(yearsAgo.getFullYear() - args.years);
-        filter.dt_iso = { $gte: yearsAgo.toISOString() };
-    }
-
+    if (args.startDate) filter.dt_iso = { $gte: new Date(args.startDate).toISOString() };
+    if (args.endDate) filter.dt_iso = { $lte: new Date(args.endDate).toISOString() };
     return filter;
 };
 
+// Data Fetching Functions
+const fetchCities = async () => {
+    const collections = await db.listCollections().toArray();
+    return collections.map(collection => collection.name);
+}
 
+// Data Fetching Functions
 const fetchWeatherData = async (args: QueryArgs) => {
     try {
-    const filter = buildFilter(args);
-
-    return await db.collection(args.city)
-        .find(filter)
-        .limit(args.limit || 10)
-        .skip(args.skip || 0)
-        .map(item => {
-            if (item.rain && item.rain['1h']) {
-                item.rain.duration_1h = item.rain['1h'];
-                delete item.rain['1h'];
-            }
-            return item;
-        })
-        .toArray();
-     } catch (error) {
+        const filter = buildFilter(args);
+        return await db.collection(args.city)
+            .find(filter)
+            .limit(args.limit || DEFAULT_LIMIT)
+            .skip(args.skip || 0)
+            .map(item => {
+                if (item.rain && item.rain['1h']) {
+                    item.rain.duration_1h = item.rain['1h'];
+                    delete item.rain['1h'];
+                }
+                return item;
+            })
+            .toArray();
+    } catch (error) {
         console.error("Error fetching weather data:", error);
         throw error;
     }
 };
 
-const fetchCities = async () => {
-    const collections = await db.listCollections().toArray();
-    return collections.map(coll => coll.name);
-}
-
-const getAverageTemperatureOverYears = async (args: QueryArgs) => {
+const aggregateAverage = async (args: QueryArgs, field: string) => {
     const filter = buildFilter(args);
+
+    // Extract the last component of the field for naming (e.g., "main.feels_like" -> "feels_like")
+    const fieldName = field.split('.').pop();
+
+    // Create the aggregation field name
+    const aggregationFieldName = `avg${fieldName.charAt(0).toUpperCase() + fieldName.slice(1)}`;
+
+    const aggregationField = {};
+    aggregationField[aggregationFieldName] = { $avg: `$${field}` };
 
     const data = await db.collection(args.city)
         .aggregate([
             { $match: filter },
-            { $group: { _id: null, avgTemp: { $avg: "$main.temp" } } }
-        ])
-        .toArray();
-
-    return data.length > 0 ? { averageTemperature: data[0].avgTemp } : null;
-};
-
-const getSeasonalAverageOverYears = async (args: QueryArgs) => {
-    const filter = buildFilter(args);
-
-    return await db.collection(args.city)
-        .aggregate([
-            {
-                $addFields: {
-                    convertedDate: {
-                        $dateFromString: {
-                            dateString: "$dt_iso",
-                            timezone: "UTC"
-                        }
-                    }
-                }
-            },
-            { $match: filter },
             {
                 $group: {
-                    _id: {
-                        season: {
-                            $switch: {
-                                branches: [
-                                    { case: { $lte: [{ $dayOfYear: "$convertedDate" }, 80] }, then: "Summer" },
-                                    { case: { $lte: [{ $dayOfYear: "$convertedDate" }, 172] }, then: "Autumn" },
-                                    { case: { $lte: [{ $dayOfYear: "$convertedDate" }, 264] }, then: "Winter" },
-                                    { case: { $lte: [{ $dayOfYear: "$convertedDate" }, 355] }, then: "Spring" },
-                                ],
-                                default: "Summer"
-                            }
-                        }
-                    },
-                    avgTemp: { $avg: "$main.temp" },
+                    _id: null,
+                    ...aggregationField,
+                    count: { $sum: 1 }
                 }
             }
         ])
         .toArray();
+
+    return data.length > 0 ? { [fieldName]: data[0][aggregationFieldName], count: data[0].count } : null;
 };
 
+const getAverageTemperature = async (args: QueryArgs) => {
+    const result = await aggregateAverage(args, "main.temp");
+    if (result && result.temp) {
+        return { averageTemperature: result.temp, count: result.count };
+    } else {
+        return { averageTemperature: 0, count: 0 }; // Default values
+    }
+};
+
+const getAverageFeelsLike = async (args: QueryArgs) => {
+    const result = await aggregateAverage(args, "main.feels_like");
+    if (result && result.feels_like) {
+        return { averageFeelsLike: result.feels_like, count: result.count };
+    } else {
+        return { averageFeelsLike: 0, count: 0 }; // Default values
+    }
+};
 
 // GraphQL Typedefs
 export const weatherTypeDefs = gql`
-type Weather {
-    dt: Int!
-    dt_iso: String!
-    timezone: Int!
-    main: Main!
-    clouds: Clouds!
-    weather: [WeatherInfo!]!
-    rain: Rain
-    wind: Wind!
-    lat: Float!
-    lon: Float!
-    city_name: String!
-}
+    type Weather {
+        dt: Int!
+        dt_iso: String!
+        timezone: Int!
+        main: Main!
+        clouds: Clouds!
+        weather: [WeatherInfo!]!
+        rain: Rain
+        wind: Wind!
+        lat: Float!
+        lon: Float!
+        city_name: String!
+    }
 
-type Main {
-    temp: Float!
-    temp_min: Float!
-    temp_max: Float!
-    feels_like: Float!
-    pressure: Int!
-    humidity: Int!
-    dew_point: Float!
-}
+    type Main {
+        temp: Float!
+        temp_min: Float!
+        temp_max: Float!
+        feels_like: Float!
+        pressure: Int!
+        humidity: Int!
+        dew_point: Float!
+    }
 
-type Stats {
-    averageTemperature: Float!
-}
+    type AverageTemperature {
+        averageTemperature: Float!
+        count: Int!
+    }
 
-type SeasonalStats {
-    season: String!
-    averageTemperature: Float!
-}
+    type AverageFeelsLike {
+        averageFeelsLike: Float!
+        count: Int!
+    }
 
-type Clouds {
-    all: Int!
-}
+    type Clouds {
+        all: Int!
+    }
 
-type WeatherInfo {
-    id: Int!
-    main: String!
-    description: String!
-    icon: String!
-}
+    type WeatherInfo {
+        id: Int!
+        main: String!
+        description: String!
+        icon: String!
+    }
 
-type Rain {
-    duration_1h: Float!
-}
+    type Rain {
+        duration_1h: Float!
+    }
 
-type Wind {
-    speed: Float!
-    deg: Int!
-}
+    type Wind {
+        speed: Float!
+        deg: Int!
+    }
+    type Query {
+        getAllCities: [String!]!
 
-type Query {
-    getWeather(city: String!, dt_iso: String, startDate: String, endDate: String, limit: Int = 10, skip: Int = 0): [Weather!]!
-    getAllCities: [String!]!
-    getAverageTemperatureOverYears(city: String!, years: Int = 0): Stats!
-    getSeasonalAverageOverYears(city: String!, years: Int = 0): [SeasonalStats!]!
-}
+        getWeather(
+            city: String!,
+            startDate: String,
+            endDate: String,
+            limit: Int = 10,
+            skip: Int = 0
+        ): [Weather!]!
+
+        getAverageTemperature(
+            city: String!,
+            startDate: String,
+            endDate: String
+        ): AverageTemperature!
+
+        getAverageFeelsLike(
+            city: String!,
+            startDate: String,
+            endDate: String
+        ): AverageFeelsLike!
+    }
 `;
 
 interface QueryArgs {
     city: string;
+    season?: string;
     limit?: number;
     skip?: number;
-    dt_iso?: string;
     startDate?: string;
     endDate?: string;
-    years?: number;
-}
+};
 
 const resolvers = {
     Query: {
-        getWeather: (_, args: QueryArgs) => fetchWeatherData(args),
         getAllCities: fetchCities,
-        getAverageTemperatureOverYears: (_, args: QueryArgs) => getAverageTemperatureOverYears(args),
-        getSeasonalAverageOverYears: (_, args: QueryArgs) => getSeasonalAverageOverYears(args)
+        getWeather: (_, args: QueryArgs) => fetchWeatherData(args),
+        getAverageTemperature: (_, args: QueryArgs) => getAverageTemperature(args),
+        getAverageFeelsLike: (_, args: QueryArgs) => getAverageFeelsLike(args),
     },
 };
 
+
+// Server Initialization
 async function startServer() {
     const typeDefs = [weatherTypeDefs];
     const server = new ApolloServer({ typeDefs, resolvers });
-
     const { url } = await startStandaloneServer(server, { listen: { port: PORT } });
     console.log(`Server ready at: ${url}`);
-
     ['SIGTERM', 'SIGINT'].forEach(signal => {
         process.on(signal as any, async () => {
             console.log(`${signal} signal received. Shutting down gracefully...`);
@@ -452,6 +460,7 @@ wait_for_mongodb() {
 
   for ((i = 1; i <= $retries; i++)); do
     if docker exec graphql-server-mongo-1 mongosh --quiet --eval "db.version()" &>/dev/null; then
+      docker exec graphql-server-mongo-1 mongosh --quiet --eval "disableTelemetry()"
       return 0
     fi
     echo "Waiting for MongoDB to start... ($i/$retries)"
---
+++ setup_scraper.sh
@@ -0,0 +1,267 @@
+#!/bin/bash
+set -e
+
+# Define the Python script filename
+PYTHON_SCRAPER_FILENAME="web_scraper.py"
+
+# Function to check if a command is available
+check_command() {
+    if ! command -v "$1" &> /dev/null; then
+        echo "Error: $1 is not installed or not available in PATH."
+        exit 1
+    fi
+}
+
+# Check if pip3 and python3 are installed and available
+check_command pip3
+check_command python3
+
+# Function to check and install required Python packages
+check_and_install_python_packages() {
+    installed_packages=$(pip3 freeze)
+    for package in "$@"; do
+        if echo "$installed_packages" | grep -q "^$package="; then
+            echo "$package is already installed."
+        else
+            echo "Installing Python package: $package"
+            pip3 install $package
+        fi
+    done
+}
+
+# Trap function to handle user interrupts
+handle_interrupt() {
+    echo -e "\nUser interrupt detected. Cleaning up..."
+    if [[ -f $PYTHON_SCRAPER_FILENAME ]]; then
+        rm $PYTHON_SCRAPER_FILENAME
+    fi
+    exit 1
+}
+
+# Set the trap for SIGINT
+trap handle_interrupt SIGINT
+
+# Function to create urls.json with a list of URLs
+create_urls_json() {
+    cat <<EOL > urls.json
+[
+    "https://www.abs.gov.au"
+]
+EOL
+}
+
+# Function to write the Python scraper to a file
+write_python_scraper_to_file() {
+    cat <<EOL > $PYTHON_SCRAPER_FILENAME
+import os
+import time
+import json
+import requests
+import logging
+import hashlib
+import threading
+import queue
+import datetime
+from bs4 import BeautifulSoup
+from urllib.parse import urljoin, urlparse
+from robotexclusionrulesparser import RobotExclusionRulesParser
+from collections import deque
+
+# Configure logging settings
+logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(levelname)s - %(message)s', filename='web_scraper.log', filemode='w')
+
+class WebScraper:
+    def __init__(self, user_agent, delay=2, download_folder="downloaded_files"):
+        # Initializer for the WebScraper class
+        self.USER_AGENT = user_agent  # User agent for the web requests
+        self.DELAY = delay  # Delay between requests
+        self.DOWNLOAD_FOLDER = download_folder  # Directory to save downloaded files
+        self.visited_links = set()  # Set to keep track of visited links
+        self.visited_domains = set()  # Set to keep track of visited domains
+        self.robots_parser = RobotExclusionRulesParser()  # Parser for robots.txt
+        self.start_time = None  # Time when the scraping started
+        self.pages_crawled = 0  # Counter for pages crawled
+        self.lock = threading.Lock()  # Lock for thread safety
+
+    def worker(self, url_queue):
+        # Worker function for threads
+        while not url_queue.empty():
+            try:
+                url = url_queue.get_nowait()
+            except queue.Empty:
+                break
+            self.crawl_sublinks(url, urlparse(url).netloc)
+            url_queue.task_done()
+
+    def estimate_time_remaining(self, total_links):
+        # Estimate the time remaining for the scraping process
+        if not self.start_time:
+            return "Estimation not available yet."
+
+        elapsed_time = time.time() - self.start_time
+        average_time_per_page = elapsed_time / self.pages_crawled if self.pages_crawled else 0
+        estimated_remaining_time = average_time_per_page * (total_links - self.pages_crawled)
+
+        # Convert estimated time from seconds to a more readable format
+        estimated_time_str = str(datetime.timedelta(seconds=int(estimated_remaining_time)))
+
+        return f"Crawled {self.pages_crawled} out of an estimated {total_links} pages. Estimated time remaining: {estimated_time_str}."
+
+    def fetch_data_from_url(self, url):
+        # Fetch data from a given URL
+        headers = {"User-Agent": self.USER_AGENT}
+        try:
+            start_time = time.time()
+            response = requests.get(url, headers=headers)
+            response.raise_for_status()
+            elapsed_time = time.time() - start_time
+            logging.info(f"Fetched data from {url} in {elapsed_time:.2f} seconds")
+            return response.text
+        except requests.RequestException as e:
+            logging.error(f"Error fetching data from {url}: {e}")
+            return ""
+
+    def extract_xlsx_links(self, html_content, base_url):
+        # Extract .xlsx links from the HTML content
+        soup = BeautifulSoup(html_content, 'html.parser')
+        return [urljoin(base_url, a['href']) for a in soup.find_all('a', href=True)
+                if a['href'].endswith('.xlsx') and self.robots_parser.is_allowed(self.USER_AGENT, a['href'])]
+
+    def compute_md5(self, filename):
+        """Compute the MD5 hash of a file."""
+        hash_md5 = hashlib.md5()
+        with open(filename, "rb") as f:
+            for chunk in iter(lambda: f.read(4096), b""):
+                hash_md5.update(chunk)
+        return hash_md5.hexdigest()
+
+    def download_file(self, url):
+        # Download a file from a given URL
+        local_filename = os.path.join(self.DOWNLOAD_FOLDER, url.split('/')[-1])  # Define local_filename
+        if not os.path.exists(self.DOWNLOAD_FOLDER):
+            os.makedirs(self.DOWNLOAD_FOLDER)
+            logging.info(f"Created download directory: {self.DOWNLOAD_FOLDER}")
+        if os.path.exists(local_filename):
+            # Check if the file on the server has the same MD5 hash as the local file
+            response = requests.head(url)
+            if 'Content-MD5' in response.headers:
+                server_md5 = response.headers['Content-MD5']
+                local_md5 = self.compute_md5(local_filename)
+                if server_md5 == local_md5:
+                    logging.info(f"File {local_filename} already exists and is identical to the server version. Skipping download.")
+                    return
+            else:
+                logging.info(f"File {local_filename} already exists. Skipping download.")
+                return
+        try:
+            with requests.get(url, stream=True) as response:
+                response.raise_for_status()
+                with open(local_filename, 'wb') as f:
+                    for chunk in response.iter_content(chunk_size=8192):
+                        f.write(chunk)
+            logging.info(f"Downloaded {url} to {local_filename}")
+        except requests.RequestException as e:
+            logging.error(f"Error downloading {url}: {e}")
+
+    def crawl_sublinks(self, start_url, domain):
+        # Crawl sublinks of a given URL
+        if not self.start_time:
+            self.start_time = time.time()
+
+        queue = deque([start_url])
+        total_links = len(queue)
+        while queue:
+            url = queue.popleft()
+            if url in self.visited_links:
+                logging.info(f"URL {url} has already been visited. Skipping.")
+                continue
+            self.visited_links.add(url)
+            html_content = self.fetch_data_from_url(url)
+
+            self.pages_crawled += 1
+            total_links = max(total_links, len(queue) + self.pages_crawled)  # Update total_links with the maximum seen so far
+            logging.info(self.estimate_time_remaining(total_links))
+
+            try:
+                soup = BeautifulSoup(html_content, 'lxml')
+            except Exception as e:
+                logging.error(f"Error parsing content from {url}: {e}")
+                continue
+            for a in soup.find_all('a', href=True):
+                link = urljoin(url, a['href'])
+                parsed_link = urlparse(link)
+                if parsed_link.netloc == domain and link not in self.visited_links:
+                    if link.endswith('.xlsx') and self.robots_parser.is_allowed(self.USER_AGENT, link):
+                        self.download_file(link)
+                    elif not link.endswith('.xlsx'):
+                        queue.append(link)
+            time.sleep(self.DELAY)
+
+    def main(self, urls):
+        # Main function to start the scraping process
+        for url in urls:
+            parsed_url = urlparse(url)
+            domain = parsed_url.netloc
+            if domain not in self.visited_domains:
+                robots_url = urljoin(url, "/robots.txt")
+                self.robots_parser.fetch(robots_url)
+                if self.robots_parser.get_crawl_delay("*"):
+                    self.DELAY = max(self.DELAY, self.robots_parser.get_crawl_delay("*"))
+                self.visited_domains.add(domain)
+                time.sleep(self.DELAY)
+
+        url_queue = queue.Queue()
+        for url in urls:
+            url_queue.put(url)
+
+        # Start threads for each URL
+        threads = []
+        for url in urls:
+            thread = threading.Thread(target=self.crawl_sublinks, args=(url, urlparse(url).netloc))
+            thread.start()
+            threads.append(thread)
+
+        # Wait for all threads to finish
+        for thread in threads:
+            thread.join()
+
+        logging.info(f"Visited links: {len(self.visited_links)}, Downloaded files: {len(os.listdir(self.DOWNLOAD_FOLDER))}")
+
+if __name__ == "__main__":
+    DEFAULT_USER_AGENT = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36"
+    USER_AGENT = input("Enter User-Agent (or press Enter to use default): ") or DEFAULT_USER_AGENT
+    logging.info(f"Using User-Agent: {USER_AGENT}")
+
+    try:
+        with open('urls.json', 'r') as f:
+            urls = json.load(f)
+            if not urls:
+                logging.error("The urls.json file is empty. Exiting.")
+                exit(1)
+    except (FileNotFoundError, json.JSONDecodeError) as e:
+        logging.error(f"Error reading or parsing urls.json: {e}")
+        print("Please ensure that the urls.json file exists and is correctly formatted.")
+        exit(1)
+
+    scraper = WebScraper(user_agent=USER_AGENT)
+    scraper.main(urls)
+EOL
+      echo "Python scraper written to $PYTHON_SCRAPER_FILENAME"
+}
+
+# Create urls.json with a list of URLs
+create_urls_json
+
+# Check and install required packages
+check_and_install_python_packages requests beautifulsoup4 robotexclusionrulesparser lxml
+
+# Write the Python scraper script
+write_python_scraper_to_file
+
+# Execute the Python scraper
+python3 $PYTHON_SCRAPER_FILENAME
+
+# Remove the temporary Python script after execution
+rm $PYTHON_SCRAPER_FILENAME
+
+echo "Done!"
